ebc1bd58e4269e6abaa3d7628285089458377534
Macsim <podryazhchik@mail.ru>
2023-11-02 13:30:02 +0300

    Add lang support, removed standard comments from template code, and more...

--- devbot/main.py
+++ devbot/main.py
@@ -1,17 +1,15 @@
 import asyncio
-import logging
-import sys
-from os import getenv, path
+from os import getenv
 from dotenv import load_dotenv
+from .utils import get_lang
 
 from aiogram import Bot, Dispatcher, Router, types
 # from aiogram.types import InlineQuery, InlineQueryResultArticle
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, BotCommand
 from aiogram.filters import Command
 # from aiogram.utils.markdown import hbold, hitalic, hcode, hlink, hpre, hstrikethrough, hunderline 
-
 router = Router()
 
 load_dotenv()
@@ -20,15 +18,19 @@ TOKEN = getenv("BOT_TOKEN")
 
 # All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
+commands = [
+    BotCommand(command="/start", description="Перезапустить бота"),
+    BotCommand(command="/source", description="Получить ссылку на исходный код бота")
+]
+lang = {}
 
 
 @router.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-
+    user_lang = message.from_user.language_code
+    strings = get_lang(user_lang)
     await message.answer(
-        f"Hello, <b>{message.from_user.first_name}</b>\n"
-        "I'm an assistant to the developers\n"
-        f"<i>only in Russian for now</i>"
+        strings["start"].format(message.from_user.first_name)
     )
 
 @router.message(Command(commands=["source"]))
@@ -41,32 +43,39 @@ async def source_cmd(message: Message) -> None:
 
 @router.message()
 async def get_query(message: Message) -> None:
+    user_lang = message.from_user.language_code
+    strings = get_lang(user_lang)
     query = lambda x: message.text.lower().startswith(x) 
     reply = message.reply_to_message
 
     if query("sticker") or query("стикер"):
         if reply:
             try:
-                await message.reply(f"Айди стикера: <code>{reply.sticker.file_id}</code>")
+                await message.reply(
+                    strings["sticker"].format(reply.sticker.file_id)
+                )
             except AttributeError:
-                await message.reply("Вы ответили НЕ НА СТИКЕР")
+                await message.reply(strings["no-sticker"])
         if not reply:
-            await message.reply("Вы не ответили на стикер / сообщение")
+            await message.reply(strings["no-reply"])
 
     if query("message") or query("сообщение") or query("смс") or query("sms"):
         if reply:
-            await message.reply(f"Тело сообщения: \n<code>{reply}</code>")
+            await message.reply(strings["sms"].format(reply))
         if not reply:
-            await message.reply("Вы не ответили на стикер / сообщение")
+            await message.reply(strings["no-reply"])
     
     if query("text") or query("текст"):
         if reply:
-            await message.reply(f"Текст: ```\n{reply.html_text}```",parse_mode="MARKDOWN")
+            await message.reply(
+                strings["text"].format(reply.html_text),
+                parse_mode="MARKDOWN"
+            )
         if not reply:
-            await message.reply("Вы не ответили на стикер / сообщение")
+            await message.reply(strings["no-reply"])
 
     if query("myid"):
-        await message.reply(f"Твой айди: <code>{message.from_user.id}</code>z")
+        await message.reply(strings["myid"].format(message.from_user.id))
 
         
     
@@ -75,6 +84,7 @@ async def main() -> None:
     # Initialize Bot instance with a default parse mode which will be passed to all API calls
     bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
     # And the run events dispatching
+    await bot.set_my_commands(commands=commands)
     dp.include_router(router=router)
     await dp.start_polling(bot)
     
---
+++ devbot/utils.py
@@ -0,0 +1,30 @@
+import yaml
+import os
+from pathlib import Path
+
+# from https://github.com/itzlayz/teagram-tl
+BASE_DIR = (
+    "/data"
+    if "DOCKER" in os.environ
+    else os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+)
+BASE_PATH = Path(BASE_DIR)
+PATH = f"{BASE_PATH}/devbot/"
+
+def get_lang(locale):  # sourcery skip: merge-duplicate-blocks
+    locale_path = f"{PATH}/locale"
+    if locale == "ru":
+        with open(f'{locale_path}/ru.yml', 'r') as f:
+            data = yaml.safe_load(f)
+
+    elif locale == "en":
+        with open(f'{locale_path}/en.yml', 'r') as f:
+            data = yaml.safe_load(f)
+
+    else:
+        with open(f'{locale_path}/en.yml', 'r') as f:
+            data = yaml.safe_load(f)
+
+    return data
+
+
